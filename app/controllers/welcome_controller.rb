33cb64396c70ab8164a757ae48e6016553705024
CK Lam <egg@CKs-MacBook-Pro.local>
2014-09-25 09:17:51 +0800

    push to heroku

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,15 @@
+class WelcomeController < ApplicationController
+
+  
+  def home
+  end
+  
+  # POST /users
+  # POST /users.json
+  def login    
+  end
+  
+
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '64fbf29ec3b17667ff22c39fc0247f7e63701f99459ee00316aa01541d4cd009ab45a20d90fdfd627314c0e4e58363c04745409aec97c4311e568611da88fc0d'
